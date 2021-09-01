2163ad3cfb6d0bafc91fdf58b38aadbdfe94dc99
Fred Ulrich <fred.ulrich@shopify.com>
2021-09-01 16:00:30 -0400

    Adding zsh syntax highlighting to the plugin install

--- install.sh
+++ install.sh
@@ -3,8 +3,11 @@
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# install powerleve10k zsh theme
+# Install Powerlevel 10k ZSH Theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+
+# Install ZSH Plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 for FILE in `ls ~/dotfiles` ; do 
   ln -sf ~/dotfiles/$FILE ~/.$FILE
